a2c4dccc9b4806822fb38d2994720138f4ee7ece
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2014-05-06 08:40:10 +0900

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by 川口功 on 2014/05/06.
+//  Copyright (c) 2014年 I.K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
